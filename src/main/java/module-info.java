e125441465050ff879a9029db890af68e7bc207d
Sadturtleman <rugsn12345@gmail.com>
2023-11-12 12:01:19 +0900

    잡다한 빌드

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,10 @@ module com.itooth {
     requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
-
+    
     opens com.itooth.controller to javafx.fxml;
     opens com.itooth to javafx.fxml;
-    
+
     exports com.itooth;
 
 }
