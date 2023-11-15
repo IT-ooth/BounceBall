7ef9c1547d0e45f0628a27701e053a63046a9c43
sunshinetoyou <victorr0618@naver.com>
2023-11-15 09:50:32 +0900

    Update Sys.java TODOLIST
    1. crase() 함수명 교체
    2. flag를 sys에서만 건드릴 수 있게 변경

--- src/main/java/com/itooth/cls/Sys.java
+++ src/main/java/com/itooth/cls/Sys.java
@@ -17,6 +17,7 @@ public class Sys{
     }
 
     // 충돌 실행 코드
+    // TODO 충돌 실행코든지 충돌 감지 후 충돌실행코드까지 연결해 주는 함수인지 구분 필요할듯
     public void crash(){
         getBall().crashed();
     }
@@ -26,6 +27,7 @@ public class Sys{
         return getBall().getBounds();
     }
     
+    // TODO 이 자체에서 flag를 false로 바꿔주면 될 것 같음
     // 스레드 종료
     public void closeThread(){
         MapController.close();
