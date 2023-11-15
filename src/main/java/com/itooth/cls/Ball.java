5238617f3a48765bce2be5e9edaf65e3f13acb01
Sadturtleman <rugsn12345@gmail.com>
2023-11-15 10:52:48 +0900

    TODO 수정
    setpos는 setcenterx, y 여러번 호출하는 것보다 한 함수로 호출하는게 더 좋다 생각함

--- src/main/java/com/itooth/cls/Ball.java
+++ src/main/java/com/itooth/cls/Ball.java
@@ -9,7 +9,7 @@ public class Ball extends Object{
     private static double time = 0;
     private static double initial_speed = 2;
     private static Circle circle;
-    private static double v; //TODO 변수명 교체 바람
+    private static double velocityX;
 
     private static final double acceleration = -10;
 
@@ -20,16 +20,15 @@ public class Ball extends Object{
         circle.setFocusTraversable(true);
 
         // KeyEvent handler
-        circle.setOnKeyPressed(e -> move(e));
-        circle.setOnKeyReleased(e -> {v = 0;});
+        circle.setOnKeyPressed(e -> moveSide(e));
+        circle.setOnKeyReleased(e -> {velocityX = 0;});
     }
 
     @Override
     public void update(){
         configUpdate();
 
-        //TODO setCenterX(), setCenterY() 로 대체 가능한듯?
-        setPos(circle.getCenterX() - v, circle.getCenterY() - calPos(time));
+        setPos(circle.getCenterX() - velocityX, circle.getCenterY() - calPos(time));
     }
 
     // config 값 업데이트
@@ -42,27 +41,23 @@ public class Ball extends Object{
         return initial_speed + acceleration * time;
     }   
 
-    //KeyEvent.pressed 일 때 실행되는 함수 TODO <- 함수명이 movement 같이 움직임 전체를 관할하는 느낌으로 바뀌면 좋을듯?
-    private void move(KeyEvent event){
-
+    private void moveSide(KeyEvent event){
         switch (event.getCode()) {
             case LEFT:
-                v = 1;
+                velocityX = 1;
                 break;
             case RIGHT:
-                v = -1;
+                velocityX = -1;
                 break;
             default: break;
         }
-
     }
 
     // 충돌 시 발생
     public void crashed(){
         time = 0;
     }
-    
-    //TODO bounds 가 있는데 setPos가 필요한가?
+
     private void setPos(double x, double y){
         circle.setCenterX(x);
         circle.setCenterY(y);
