d10dc4748d859f6a8727ddb98e2b1c3fa2d3cb27
sunshinetoyou <victorr0618@naver.com>
2023-11-15 09:48:42 +0900

    Update Block.java TODOLIST
    1. if 문 삭제 후 return 으로 교체

--- src/main/java/com/itooth/cls/Block.java
+++ src/main/java/com/itooth/cls/Block.java
@@ -5,12 +5,15 @@ import javafx.geometry.Bounds;
 public abstract class Block extends Object{
 
     //충돌 감지
-    protected boolean isCrashto(Bounds bound){
+    // TODO 불필요한 if문인듯?
+    protected boolean isCrashto(Bounds bound) {
+
         if (bound.intersects(Sys.getInstance().getBallbound())){
             return true;
         }
         else{
             return false;
         }
+
     }
 }
