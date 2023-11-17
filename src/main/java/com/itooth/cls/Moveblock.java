8d1c1a755d5298971dfc811b7308a57cad69f01e
hyeok1 <wjddlsgur0928@naver.com>
2023-11-18 03:51:58 +0900

    moveBlock 수정 및 MoveVector 추가

--- src/main/java/com/itooth/cls/Moveblock.java
+++ src/main/java/com/itooth/cls/Moveblock.java
@@ -6,30 +6,30 @@ import javafx.scene.shape.Rectangle;
 public class Moveblock extends Block{
 
     private Rectangle moveBlock;
-    private int index;
-    //moveX : moveBlock의 X좌표 움직임
-    //moveY : moveBlock의 y좌표 움직임
-    private int[] moveX = new int[]{0, 0, -1, 1};
-    private int[] moveY = new int[]{-1, 1, 0, 0};
+    private MoveVector vector;
 
     //생성자
-    public Moveblock(Rectangle block, int index){
+    public Moveblock(Rectangle block, MoveVector vector){
         moveBlock = block;   
-        this.index = index;
+        this.vector = vector;
     } 
+
+    //공이 블럭의 윗면에 충돌했을 때만 블럭이 이동하도록 bottomcrash() 오버로딩
+    @Override
+    protected void bottomCrash(){
+        Sys.getInstance().runcrash(Direction.BOTTOM);
+        move();
+    }
     
     //블럭 업데이트 함수
     @Override
     public void update() {
-        if (isCrashto(getBounds(moveBlock))){
-            move();
-            Sys.getInstance().crash();
-        };
+        runBlock(getBounds(moveBlock));
     }
 
     //블럭 움직임 구현
     private void move() {
-        moveBlock.setX(moveBlock.getX() + moveX[index]);
-        moveBlock.setY(moveBlock.getY() + moveY[index]);
+        moveBlock.setX(moveBlock.getX() + vector.dx * moveBlock.getWidth());
+        moveBlock.setY(moveBlock.getY() + vector.dy * moveBlock.getWidth());
     }
 }
