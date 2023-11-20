4d5c4ccbb1f4a3eee5bb2e9c6ca47bdb29f2f706
qofo <junseolee02@gmail.com>
2023-11-21 01:00:38 +0900

    테스트용 다음 맵 생성

---
+++ src/main/java/com/itooth/controller/Map2Controller.java
@@ -0,0 +1,55 @@
+package com.itooth.controller;
+
+import java.util.ResourceBundle;
+
+import java.net.URL;
+
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.fxml.FXML;
+
+
+import com.itooth.cls.Obstacle;
+import com.itooth.cls.Sys;
+import com.itooth.cls.Ball;
+import com.itooth.cls.Block;
+import com.itooth.cls.Star;
+import com.itooth.cls.abstractcls.Controller;
+
+public class Map2Controller extends Controller{
+
+    @FXML private Circle ball;
+    @FXML private Group stars;
+    @FXML private Group blocks;
+    @FXML private Group obstacles;
+    @FXML private Polygon star1;
+    @FXML private Polygon star2;
+
+    // 초기화
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        setInitial(ball);
+
+        //ball 객체는 sprites에서 무조건 0번째 인덱스여야 함.
+        sprites.add(new Ball(ball));
+
+        //star 객체 초기화
+        Sys.getInstance().setStarGroup(stars);
+        for (Node star: stars.getChildren()) {
+            sprites.add(new Star((Polygon)star));
+        }
+
+        for (Node block: blocks.getChildren()){
+            sprites.add(new Block(block));
+        }
+
+        for (Node obs: obstacles.getChildren()){
+            sprites.add(new Obstacle(obs));
+        }
+
+        this.start();
+    }
+
+}
